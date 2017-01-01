e87cde4f4c534df0a6d37cefb1049902f2c3ae0b
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2017-01-01 14:34:51 -0500

    dualcplex: Ability to change unit names

--- dualcplex/doc.go
+++ dualcplex/doc.go
@@ -37,3 +37,27 @@ const (
 	unit2                  = "Γ"
 	unit3                  = "iΓ"
 )
+
+var (
+	unitNames = [3]string{unit1, unit2, unit3}
+)
+
+// ResetUnitNames sets the names of the dual-complex units equal to their
+// default values.
+func ResetUnitNames() {
+	unitNames[0] = unit1
+	unitNames[1] = unit2
+	unitNames[2] = unit3
+}
+
+// SetUnitNames sets the names of the dual-complex units.
+func SetUnitNames(u1, u2, u3 string) {
+	unitNames[0] = u1
+	unitNames[1] = u2
+	unitNames[2] = u3
+}
+
+// UnitNames returns the current names of the dual-complex units.
+func UnitNames() [3]string {
+	return unitNames
+}
