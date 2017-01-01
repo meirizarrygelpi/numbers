795eef04b11ac050599dbc92594976447aa813d1
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2017-01-01 15:20:24 -0500

    supra: Ability to change unit names

--- supra/doc.go
+++ supra/doc.go
@@ -37,3 +37,27 @@ const (
 	unit2                  = "β"
 	unit3                  = "γ"
 )
+
+var (
+	unitNames = [3]string{unit1, unit2, unit3}
+)
+
+// ResetUnitNames sets the names of the supra units equal to their
+// default values.
+func ResetUnitNames() {
+	unitNames[0] = unit1
+	unitNames[1] = unit2
+	unitNames[2] = unit3
+}
+
+// SetUnitNames sets the names of the supra units.
+func SetUnitNames(u1, u2, u3 string) {
+	unitNames[0] = u1
+	unitNames[1] = u2
+	unitNames[2] = u3
+}
+
+// UnitNames returns the current names of the supra units.
+func UnitNames() [3]string {
+	return unitNames
+}
